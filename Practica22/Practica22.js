d1ffe68d4337ad23d89bdbe231f4782742c3b148
Jennilidane <lizeth.damaso@uabc.edu.mx>
2023-10-03 18:35:47 -0700

    Primer practica de javascript, utilizar variables y realizar operaciones basicas

---
+++ Practica22/Practica22.js
@@ -0,0 +1,28 @@
+// var num1=2;
+// var num2=3;
+// var resultado=num1+num2;
+// alert(resultado);
+
+// var mensaje="Bienvenido a nuestro sitio web";
+// var texto_1= "\nTexto comillas dobles";
+// var texto_2='\nTexto comillas simples';
+// alert(mensaje+texto_1+texto_2);
+
+// var clienteRegistrado=false;
+// var ivaiIncluido=true;
+// alert(ivaiIncluido);
+
+
+// var array =['Tijuana','Rosarito','Ensenada','Mexicali','Tecate'];
+// alert(array[4]);
+
+
+var num1=prompt("Ingresa el primer numero");
+var num2=prompt("Ingresa el segundo numero");
+var num1=parseInt(num1);
+var num2=parseInt(num2);
+var suma=num1+num2;
+var resta=num1-num2;
+var multi=num1*num2;
+var div=num1/num2;
+alert("Resultado de suma: "+suma+"\nResultado de resta: "+resta+"\nResultado de multiplicación: "+multi+"\nResultado de división: "+div);
