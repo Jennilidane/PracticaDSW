c7821c885b7ba9ceadcdbe5cad4c9fef12a63e0b
Jennilidane <lizeth.damaso@uabc.edu.mx>
2023-11-13 19:30:39 -0800

    Se hizo una base de datos para un registro

---
+++ Ejemplo26/Ejemplo26.js
@@ -0,0 +1,15 @@
+//  for (var i = 0; i < 10; i++)
+//  {
+//      console.log("Hola mundo\n");
+//  }
+
+//  var i=10;
+//  while(i<=20){
+//      console.log(i);
+//      i=i+2;
+//  }
+var i=0;
+do {
+     i += 1;
+    console.log(i);
+}while(i<5);
